faef53d762a6583b2044705c986d5a6a7f848aae
Sean Kelly <kelly@seankelly.biz>
2012-05-23 22:38:16 +0000

    Authenticate username + password against DMCC's SOAP pwdVerification sevice

--- src/main/java/gov/nasa/jpl/edrn/dmcc/auth/DMCCAuthenticationInterceptor.java
+++ src/main/java/gov/nasa/jpl/edrn/dmcc/auth/DMCCAuthenticationInterceptor.java
@@ -4,6 +4,8 @@
 package gov.nasa.jpl.edrn.dmcc.auth;
 
 // Apache™ DS
+import org.apache.directory.server.core.CoreSession;
+import org.apache.directory.server.core.DefaultCoreSession;
 import org.apache.directory.server.core.DirectoryService;
 import org.apache.directory.server.core.interceptor.BaseInterceptor;
 import org.apache.directory.server.core.interceptor.context.BindOperationContext;
@@ -11,14 +13,21 @@ import org.apache.directory.server.core.interceptor.NextInterceptor;
 import org.apache.directory.server.core.LdapPrincipal;
 
 // Apache™ Shared LDAP API
-import org.apache.directory.shared.ldap.util.StringTools;
+import org.apache.directory.shared.ldap.constants.AuthenticationLevel;
 import org.apache.directory.shared.ldap.name.DN;
 import org.apache.directory.shared.ldap.name.RDN;
+import org.apache.directory.shared.ldap.util.StringTools;
+
+// DMCCrap
+import org.fhcrc.perdy.edrn_ws.ws_newcompass.WsNewcompass;
+import org.fhcrc.perdy.edrn_ws.ws_newcompass.WsNewcompassSoap;
 
 // Logging
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Misc
+import java.net.URL;
 
 /**
  * An interceptor that delegates authentication to the DMCC authentication web service,
@@ -33,8 +42,20 @@ public class DMCCAuthenticationInterceptor extends BaseInterceptor {
     /** Where the authentication web service lives */
     private String authWebServiceURL = "https://www.compass.fhcrc.org/edrn_ws/ws_newcompass.asmx";
 
-    /** What RDN attribute type to look for for user IDs? */
+    /** What RDN attribute type to look for for user IDs. */
     private String rdnAttributeType = "0.9.2342.19200300.100.1.1";
+
+    /** How long of an IBM check digit string to make. */
+    private int checkDigitLength = 4096;
+
+    /** Where we keep the soap. */
+    private WsNewcompass soapService;
+
+    /** IBM check digits required for some inane reason by methods in DMCC's soap service */
+    private String checkDigits;
+
+    /** The directory service we're … uhm … "servicing" */
+    private DirectoryService directoryService;
 
     /** Get the location of the authentication web service.
      *
@@ -68,12 +89,35 @@ public class DMCCAuthenticationInterceptor extends BaseInterceptor {
         this.rdnAttributeType = rdnAttributeType;
     }
 
+    /** Get the length of the check digit string.
+     *
+     * @return The length of the check digit string.
+     */
+    public int getCheckDigitLength() {
+        return this.checkDigitLength;
+    }
+    
+    /** Set the length of the check digit string.
+     *
+     * @param checkDigitLength The new length of the check digit string.
+     */
+    public void setCheckDigitLength(int checkDigitLength) {
+        this.checkDigitLength = checkDigitLength;
+    }
+
     /**
      * The initialization method that … uhm … 
      */
     public void init(DirectoryService directoryService) throws Exception {
         super.init(directoryService);
-        // Establish connection?
+        this.directoryService = directoryService;
+        this.soapService = new WsNewcompass(new URL(this.authWebServiceURL)); // DMCC's service name is stupid.
+        LOG.debug("Created SOAP service {}", this.soapService);
+        StringBuffer b = new StringBuffer(this.checkDigitLength);
+        for (int i = 0; i < this.checkDigitLength; ++i)
+            b.append('0');
+        this.checkDigits = b.toString();
+        LOG.debug("Created {} zeros for the check digits (ugh)", this.checkDigitLength);
     }
 
     /**
@@ -84,8 +128,10 @@ public class DMCCAuthenticationInterceptor extends BaseInterceptor {
      * @return True if authentic and valid, false if not.
      */
     public boolean isAuthentic(String username, String password) throws Exception {
-        // TODO: implement this as a web service call
-        return false;
+        WsNewcompassSoap port = this.soapService.getWsNewcompassSoap12(); // DMCC's port names are stupid.
+        String response = port.pwdVerification(username, password, this.checkDigits);
+        LOG.debug("DMCC says that for username {} and password [REDACTED] the response is {}", username, response);
+        return "valid".equals(response);
     }
 
     /**
@@ -113,6 +159,11 @@ public class DMCCAuthenticationInterceptor extends BaseInterceptor {
             LOG.debug("Got uid '{}' with password 'NOPE! Not gonna show ya!'", uid);
             if (!this.isAuthentic(uid, password))
                 throw new DMCCAuthenticationException("Uid '" + uid + "' and password not valid per DMCC");
+            LOG.debug("Guess what: your DMCC username & password are OK!");
+            LdapPrincipal principal = new LdapPrincipal(opContext.getDn(), AuthenticationLevel.SIMPLE);
+            CoreSession session = new DefaultCoreSession(principal, this.directoryService);
+            opContext.setSession(session);
+            opContext.setCredentials(null);
         } catch (RuntimeException ex) {
             throw ex;
         } catch (Exception ex) {
@@ -121,4 +172,6 @@ public class DMCCAuthenticationInterceptor extends BaseInterceptor {
             next.bind(opContext);
         }
     }
+    
+    
 }
